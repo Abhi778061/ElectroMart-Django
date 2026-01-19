51a511970506cef5bfbfb4d319e75ea596fa68f0
Abhinay <abhi778061@gmail.com>
2026-01-19 15:57:31 +0530

    Fix media serving

--- Quick_cart/Quick_cart/urls.py
+++ Quick_cart/Quick_cart/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,5 +24,9 @@ urlpatterns = [
     path('', include('Click_cart.urls')),
 ]
 
+# ✅ FIX: serve media files in development
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
